713be57ba392cc8ac3e1ab269c3fb0cd6f70abea
Onur GÖKER <onurgoker89@gmail.com>
2019-03-27 18:12:38 +0300

    Vraiables are added.

---
+++ src/variables.go
@@ -0,0 +1,33 @@
+/**
+Variables Program with GoLang
+Onur GOKER
+info@onurgoker.com
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	//define primitive types
+	var newStr string = "Welcome to Variables!"
+	var count int = 1
+
+	fmt.Println(newStr + "This is your " + strconv.Itoa(count) + "st code")
+
+	//define variable without primitive types
+	var newStr2 = "For your "
+	var count2 = 2
+
+	fmt.Println(newStr2 + strconv.Itoa(count2) + "nd code, variables are defined without primitive types!")
+
+	//define variables with short hand notation
+	newStr3 := "It is also possible to define variables with short hand notation."
+	count3 := 3
+
+	fmt.Println(newStr3 + "Here you typed your " + strconv.Itoa(count3) + "rd code block!")
+}
